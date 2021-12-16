e4572e2fd8cf11a72b58317210d5643085528aa8
Bryan Gutiérrez <83380858+JDN22@users.noreply.github.com>
2021-12-16 10:26:25 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,8 +5,6 @@ import datetime
 import pandas as pd
 import numpy as np
 import altair as alt
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.model_selection import train_test_split
 from keras.models import model_from_json
 
 
